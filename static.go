b2538aa6237ec15a6c5c59cbc4d4b685d001201d
Jameson Little <t.jameson.little@gmail.com>
2015-04-09 22:40:15 -0600

    Clean url in static file handler
    - prevents escaping root

--- static.go
+++ static.go
@@ -8,7 +8,7 @@ import (
 
 func Static(root string) Middleware {
 	return func(w http.ResponseWriter, r *http.Request, d Data) bool {
-		fPath := path.Join(root, r.URL.Path)
+		fPath := path.Join(root, path.Clean(r.URL.Path))
 
 		// if the path doesn't exist, continue down the stack
 		f, err := os.Open(fPath)
